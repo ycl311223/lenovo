3fa963c5b8030bdb0ce7980f0ec97e3e8196ae9b
ycl <787607574@qq.com>
2018-09-04 23:20:36 +0800

    商品详情页面的展示完成

--- app/Http/Controllers/Home/GoodsController.php
+++ app/Http/Controllers/Home/GoodsController.php
@@ -13,10 +13,26 @@ class GoodsController extends Controller
         $goods=\DB::table('goods')->where("id",$id)->first();
         //商品图片表
         $goodsImg=\DB::table("goodsimg")->where('gid',$id)->get();
+
+        //查询商品对应的评论
+        $commonTot=\DB::table('comment')->where('gid',$id)->count();
+        $goodTot=\DB::table('comment')->where('start','>',4)->count();
+        $chaTot=\DB::table('comment')->where('start','<=',2)->count();
+        $zhongTot=$commonTot-$goodTot-$chaTot;
+
+        $arr=array(
+            "commonTot"=>$commonTot,
+            "goodTot"=>$goodTot,
+            "chaTot"=>$chaTot,
+            "zhongTot"=>$zhongTot,
+        );
+        $comment=\DB::table('comment')->where('gid',$id)->get();
         //格式化数据
         $data=array(
           "goods"=>$goods,
             "goodsImg"=>$goodsImg,
+            "arr"=>$arr,
+            "comment"=>$comment,
         );
         return view("home.goods")->with($data);
     }
